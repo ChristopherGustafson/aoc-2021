00c1b60deef2ec304838e1d4104ac7b5b692d804
ChristopherGustafson <christopher.gustafson@outlook.com>
2021-12-07 21:56:26 +0100

    Day 7, fix solution to work for all inputs

--- day07/day07.py
+++ day07/day07.py
@@ -20,11 +20,19 @@ with open("day07/input.txt") as input:
   print(f"Part 1: Fuel sum: {int(fuel_sum)}")
 
   # Part 2
-  mean_pos = math.floor((st.mean(positions)))
-  fuel_sum = 0
-  for pos in positions:
-    fuel_sum = fuel_sum + sum(range(abs(pos-(mean_pos))+1))
-  print(f"Part 2: Fuel sum: {int(fuel_sum)}")
+  mean_pos = round((st.mean(positions)))
+  min_fuel_sum = math.inf
+
+  # Calculate for fuel sum for mean +/- 1 beacuse the minimal fuel sum
+  # is found where mean - 1/2 <= position <= mean + 1/2. 
+  # Thanks u/throwaway7824365346 for the proof https://www.reddit.com/r/adventofcode/comments/rawxad/2021_day_7_part_2_i_wrote_a_paper_on_todays/ 
+  
+  for delta in range(-1,2):
+    current_fuel_sum = 0
+    for pos in positions:
+      current_fuel_sum = current_fuel_sum + sum(range(abs(pos-(mean_pos+delta))+1))
+    if current_fuel_sum < min_fuel_sum: min_fuel_sum = current_fuel_sum  
+  print(f"Part 2: Fuel sum: {int(min_fuel_sum)}")
 
   end_time = time.time()
 
